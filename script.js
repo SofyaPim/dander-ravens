6cffc8994087df90b4b0bf08f5ada3c80c799240
Sofya <48826470+SofyaPim@users.noreply.github.com>
2025-05-30 15:49:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ collisionCanvas.height = window.innerHeight;
 
 let score = 0;
 let gameOver = false;
-const fontSize = window.innerWidth / 10;
+const fontSize = window.innerWidth / 20;
 ctx.font = `bold ${fontSize}px Shantell Sans`;
 
 let timeToNextRaven = 0;
